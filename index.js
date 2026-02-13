66ca76a6e6bd3b2f262e2eb304e7bde3a737e717
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-13 18:05:08 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// Global variable
+const distanceInMiles = 100;
+
+// Function that calculates estimated arrival time
+function estimatedArrivalTime() {
+  // Local variable
+  const milesPerHour = 50;
+  // Return distance divided by speed
+  return distanceInMiles / milesPerHour;
+}
+
+// Assign the result to the HTML element
+document.getElementById('result').innerHTML = estimatedArrivalTime();
 
